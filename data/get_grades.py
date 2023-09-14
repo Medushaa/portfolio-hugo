bf8e2be013c0075076cab94215c44104ab918848
Masuda <Masudarahman27@gmail.com>
2023-09-15 00:42:33 +0600

    grades

--- data/get_grades.py
+++ data/get_grades.py
@@ -3,17 +3,29 @@ import csv
 cse = []
 mat = []
 others = []
-grades = []
+grades = {'A+':0, 'A':0, 'A-':0, 'B+':0, 'B':0}
 cgpa = []
 
 output = open("output_grades.txt","w")
+
+links = {}
+with open('note-links.csv') as file_obj:
+    reader_obj = csv.reader(file_obj)
+
+    for row in reader_obj:
+        links[row[0]] = row[1]
+
 
 with open('grades.csv') as file_obj:
     reader_obj = csv.reader(file_obj)
 
     for row in reader_obj:
-        grades.append(row[2])
+        grades[row[2]] += 1
         cgpa.append(float(row[3]))
+        if row[0] in links.keys():
+            row.append(links[row[0]])
+        else:
+            row.append("None")
         
         if len(row[1])>30:
             row[1] = row[1][:30] + "..."
@@ -30,7 +42,11 @@ with open('grades.csv') as file_obj:
 
 def show_table(courses):
     for row in courses:
-        output.write("   | " + " | ".join(row) + " |\n")
+        if row[4] == "None":
+            output.write("   | " + " | ".join(row[:4]) + " | (No Notes) |\n")
+        else:
+            output.write("   | " + " | ".join(row[:4]) + " | [Note Link](" + row[4] + ") |\n")
+
 
 
 output.write("###CSE courses###\n")
@@ -43,3 +59,6 @@ output.write("###GenEd courses###\n")
 show_table(others)
 
 output.write("\nOverall cgpa: " + str(sum(cgpa) / len(cgpa)) + " and Total number of course: "+str(len(cgpa)))
+
+print(grades)
+
